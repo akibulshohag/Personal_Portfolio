26a965e41e6c9adf8ba631521bb5cc0e229f1b66
akibulshohag <akibulshohag@gmail.com>
2022-07-02 17:58:26 +0600

    02-07-22

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,18 +1,18 @@
-import React, { useContext } from "react";
-import "./Intro.css";
+import { motion } from "framer-motion";
+import { useContext } from "react";
+import { Link } from "react-scroll";
+import { themeContext } from "../../Context";
+import boy from "../../img/boy.png";
+import crown from "../../img/crown.png";
+import Github from "../../img/github.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import Instagram from "../../img/instagram.png";
+import LinkedIn from "../../img/linkedin.png";
+import thumbup from "../../img/thumbup.png";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
-import glassesimoji from "../../img/glassesimoji.png";
-import thumbup from "../../img/thumbup.png";
-import crown from "../../img/crown.png";
 import FloatinDiv from "../FloatingDiv/FloatingDiv";
-import Github from "../../img/github.png";
-import LinkedIn from "../../img/linkedin.png";
-import Instagram from "../../img/instagram.png";
-import { themeContext } from "../../Context";
-import { motion } from "framer-motion";
-import { Link } from "react-scroll";
+import "./Intro.css";
 const Intro = () => {
   // Transition
   const transition = { duration: 2, type: "spring" };
@@ -28,7 +28,7 @@ const Intro = () => {
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Andrew Thomas</span>
+          <span>Akibul Islam</span>
           <span>
             Frontend Developer with high level of experience in web designing
             and development, producting the Quality work
